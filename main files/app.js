d3d908a23139eb5853f7e551475349f149d01b9f
mubaraq <barakkenny500@gmail.com>
2023-11-22 23:52:21 +0100

    Done with the user Interface of the Setup Guide.

--- main files/app.js
+++ main files/app.js
@@ -1,6 +1,6 @@
-// const setUp = document.querySelector(".setup__guide__questions");
-// const arrowDown = document.querySelector(".bx-chevron-down");
+const setUp = document.querySelector(".setup__guide__questions");
+const arrowDown = document.querySelector(".bx-chevron-down");
 
-// arrowDown.addEventListener("click", () => {
-//   setUp.classList.toggle("active");
-// });
+arrowDown.addEventListener("click", () => {
+  setUp.classList.toggle("active");
+});
